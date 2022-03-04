28eb721ae05e3be4d44f68157273ebec36dbaab0
jog1997 <100663234+jog1997@users.noreply.github.com>
2022-03-04 12:12:23 -0600

    Create SQL script for owner table.sql
    Added mySQL file to create table.

---
+++ SQL script for owner table.sql
@@ -0,0 +1,19 @@
+SELECT * FROM puppy.owner;
+##Trial to check if it works
+INSERT INTO owner
+VALUES ('1','Jose','1');
+
+##Create the rest of the owners
+
+INSERT INTO owner
+VALUES
+	('2', 'Josh', '2'),
+    ('3', 'Juan', '3'),
+    ('4', 'Giselle', '4'),
+    ('5', 'Ivan', '5'),
+    ('6', 'David', '6'),
+    ('7', 'Jasmine', '7'),
+    ('8', 'Edgar', '9'),
+    ('9', 'Garret', '10'),
+    ('10', 'Kevin', '11');
+    
